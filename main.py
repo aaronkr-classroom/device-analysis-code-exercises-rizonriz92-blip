6c302d3a02878e6eeefc0d53974835361b8925af
rizonriz92-blip <rizonriz92@gmail.com>
2026-04-09 15:42:36 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+
+#main.py
+
+from sensor import TemperatureSensor, LightSensor
+
+temp = TemperatureSensor("Temp1")
+light = LightSensor("Light1")
+
+print(f"Temp: {temp.read()}")
+print(f"Light: {light.read()}")
