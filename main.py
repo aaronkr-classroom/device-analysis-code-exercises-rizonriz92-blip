94cd888872192195ba4c03bb4fb81267cbcd3bc2
rizonriz92-blip <rizonriz92@gmail.com>
2026-04-23 14:29:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from sensor import TemperatureSensor, LightSensor
 
 temp = TemperatureSensor("Temp1")
 light = LightSensor("Light1")
+name = NameSensor("name")
+humidity = HumiditySensor("humidity")
 
 print(f"Temp: {temp.read()}")
 print(f"Light: {light.read()}")
+print(f"Name: {name.read()}")
+print(f"Humidity: {humidity.read()}")
